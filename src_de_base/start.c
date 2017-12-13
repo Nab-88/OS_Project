1f80c51927e2c2280dabefddb0cc0e23278c0930
Nab-88 <nicolas.bernard38460@gmail.com>
2017-12-13 14:46:27 +0100

    version de lexo avec les 2 processus qui fonctionnent

--- src_de_base/start.c
+++ src_de_base/start.c
@@ -2,22 +2,48 @@
 #include <inttypes.h>
 #include "ecran.h"
 #include "temps.h"
+#include "processus.h"
+#include "malloc.c.h"
 void traitant_IT_32(void);
+void ctx_sw(uint32_t *, uint32_t *);
 
-// on peut s'entrainer a utiliser GDB avec ce code de base
-// par exemple afficher les valeurs de x, n et res avec la commande display
+struct processus* table_proc[2];
+uint32_t pid_courant = 0;
 
-// une fonction bien connue
-uint32_t fact(uint32_t n)
-{
-    uint32_t res;
-    if (n <= 1) {
-        res = 1;
-    } else {
-        res = fact(n - 1) * n;
-    }
-    return res;
+void idle(void){
+  printf("[idle] : je tente de passer la main a proc1 ...\n");
+  ctx_sw(table_proc[0] -> registres, table_proc[1] -> registres);
 }
+
+void proc1(void){
+  printf("[proc1] : idle m'a passe la main\n");
+  printf("[proc1] : j'arrete le systeme\n");
+  hlt();
+}
+
+int creer_processus(void (*code)(void), char * nom){
+  struct processus * proc = malloc(sizeof(struct processus));
+  if (pid_courant == 0) {
+    proc ->pid = pid_courant;
+    strcpy(proc->nom, nom);
+    proc -> p_etat = ELU;
+    table_proc[0] = proc;
+  }
+  else {
+    proc ->pid = pid_courant;
+    strcpy(proc->nom, nom);
+    proc -> p_etat = ACTIVABLE;
+    table_proc[1] = proc;
+    proc -> pile_exec[511] = (uint32_t) code;
+    proc -> registres[1]= (uint32_t) &(proc->pile_exec[511]);
+    table_proc[1] = proc;
+  }
+  pid_courant ++;
+  return pid_courant - 1;
+}
+
+
+
 
 void kernel_start(void)
 {
@@ -28,9 +54,12 @@ void kernel_start(void)
     init_traitant_IT(32, traitant_IT_32);
     // printf("B\n");
     masquage_IRQ(0, false);
-    sti();
-    while (1) {
-        // cette fonction arrete le processeur
-        hlt();
-    }
+    creer_processus(&idle, "idle");
+    creer_processus(&proc1, "proc1");
+    idle();
+    // sti();
+    // while (1) {
+    //     // cette fonction arrete le processeur
+    //     hlt();
+    // }
 }
